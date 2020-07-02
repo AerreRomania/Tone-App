733516ec1b1fd1c8eccfd322850c38f4f95d9830
jurjsergiu <jurjsergiu@gmail.com>
2020-07-02 11:14:29 +0300

    Tone App
    Initial commit

---
+++ Tone/Tone.Android/Render/CustomEntryAndroid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Tone.Droid.Renderer;
+using Tone.Render;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryAndroid))]
+namespace Tone.Droid.Renderer
+{
+   public class CustomEntryAndroid:EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(0f);
+                gradientDrawable.SetStroke(2, Android.Graphics.Color.ParseColor("#06bbdc"));
+                gradientDrawable.SetColor(Android.Graphics.Color.White);
+                Control.SetBackground(gradientDrawable);
+                Control.SetPadding(40,25,30,30);
+            }
+        }
+    }
+}
---
+++ Tone/Tone.iOS/Render/CustomEntryiOS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Foundation;
+using Tone.iOS.Renderer;
+using Tone.Render;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryiOS))]
+namespace Tone.iOS.Renderer
+{
+    class CustomEntryiOS: EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Layer.CornerRadius = 10;
+                Control.Layer.BorderWidth = 2f;
+                Control.Layer.BorderColor = Color.FromHex("#06bbdc").ToCGColor();
+                Control.BackgroundColor = Color.White.ToUIColor();
+                //Control.LeftView = new UIKit.UIView(new CGRect(0, 0, 10, 0));
+                //Control.LeftViewMode = UIKit.UITextFieldViewMode.Always;
+            }
+           
+        }
+    }
+}
